b4a13a31d9fae9479452388c483054e1f820f0cf
Giozsv <giovannabarbosa334@gmail.com>
2022-02-24 17:18:10 -0300

    java script

--- teste.js
+++ teste.js
@@ -1,15 +1,33 @@
 //MANIPULANDO ARQUIVOS
 
-//var preco = 2;
-var desconto = 0.2;
-const PRECO = 2;
-var total = PRECO - desconto;
+// //var preco = 2;
+// var desconto = 0.2;
+// const PRECO = 2;
+// var total = PRECO - desconto;
 
-//FUNÇOES
+// //FUNÇOES
 
-function soma(a, b) {
-  console.log(a + b);       //verificar se funciona
-   return a + b;           //retornar valor da funçao
+// function soma(a, b) {
+//   console.log(a + b);       //verificar se funciona
+//    return a + b;           //retornar valor da funçao
+// }
+
+// soma(3, 5);  //8
+
+// console.log("Hello world!");
+
+function returnEvenValues(){  //retornar  nº pares
+  let evenNums = [];
+  for (let i = 0; i < array.length; i++) {
+     if(array[i] % 2 === 0) {
+       evenNums.push(array[i]);
+     } else {
+      console.log(`${array[i]} não é par!`);        //se p nº não satisfaz
+     }
+   }
+   console.log('Os números pares são:', evenNums);
 }
 
-soma(3, 5);  //8
+let array = [1, 2, 4, 5, 7, 8];
+
+returnEvenValues(array);  
