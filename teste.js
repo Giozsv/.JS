0a98c6f9c7d76f5553c9d3868e3d77357c257269
Giozsv <giovannabarbosa334@gmail.com>
2022-02-24 16:31:38 -0300

    javascript

--- teste.js
+++ teste.js
@@ -1,2 +1,15 @@
-// var y = 0;
-var x = 1;
+//MANIPULANDO ARQUIVOS
+
+//var preco = 2;
+var desconto = 0.2;
+const PRECO = 2;
+var total = PRECO - desconto;
+
+//FUNÇOES
+
+function soma(a, b) {
+  console.log(a + b);       //verificar se funciona
+   return a + b;           //retornar valor da funçao
+}
+
+soma(3, 5);  //8
