51e51982003e3b96198adf9e70d289600aad7db1
Giozsv <giovannabarbosa334@gmail.com>
2022-02-25 10:18:15 -0300

    script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+var currentNumberWrapper = document.getElementById("currentNumber")
+var currentNumber = 0;
+
+function increment() {
+  currentNumber = currentNumber + 3;
+  currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+  currentNumber = currentNumber - 3;
+  currentNumberWrapper.innerHTML = currentNumber;
+}
